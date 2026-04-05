d253cbfac7e7986cb2cdd8b18365638694a17889
Maximetra <maximembiyakiaku@gmail.com>
2026-04-05 16:11:11 +0200

    Change database settings

--- wuwa/settings.py
+++ wuwa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -83,13 +84,11 @@ WSGI_APPLICATION = 'wuwa.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DATABASE'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-    }
+    'default': dj_database_url.config(
+        # On dit explicitement à Django de chercher la variable DATABASE_URL de Neon
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
